187c3dae8399dc863668eb60f27278ce7be600a6
Kagheni <Kaghenimbale@gmail.com>
2023-03-15 11:11:37 +0200

    Call the InputValue function in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './index.css';
+import getData from './modules/items.js';
+import InputValues from './modules/submitData.js';
+
+const refreshBtn = document.getElementById('refresh');
+const form = document.getElementById('form');
+
+refreshBtn.addEventListener('click', getData);
+
+form.addEventListener('submit', InputValues);
